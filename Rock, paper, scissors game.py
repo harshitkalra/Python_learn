502bedfad63b76e819ee602088c7bbddc9b44dcf
Harshit <user@MacBook-Pro.local>
2025-09-27 15:20:35 +0900

    initial commit

---
+++ Rock, paper, scissors game.py
@@ -0,0 +1,5 @@
+import random
+
+roll = random.randint(1,6)
+
+print("The computer rolls a " + str(roll))
---
+++ tax.py
@@ -0,0 +1,7 @@
+amount = 200
+tax = 0.07
+total = amount + amount*tax
+print(total)
+
+name = 'HArshit ¥n kalra'
+print(name)
