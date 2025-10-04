33b6d7599644b6e558e2cad4a09aa448d0b4360b
Harshit <user@MacBook-Pro.local>
2025-10-05 08:08:11 +0900

    Python

--- Rock, paper, scissors game.py
+++
@@ -1,5 +0,0 @@
-import random
-
-roll = random.randint(1,6)
-
-print("The computer rolls a " + str(roll))
--- tax.py
+++
@@ -1,7 +0,0 @@
-amount = 200
-tax = 0.07
-total = amount + amount*tax
-print(total)
-
-name = 'HArshit ¥n kalra'
-print(name)
